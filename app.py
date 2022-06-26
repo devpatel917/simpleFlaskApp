cf60b4e743dd402f2cf95cd078fde771038039b5
Dev Patel <68211801+devpatel917@users.noreply.github.com>
2022-06-26 00:59:08 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,9 @@
-from flask import Flask, render_template, Response
-import cv2
+from flask import Flask, render_template
+
 
 
 app = Flask(__name__)
-# camera = cv2.VideoCapture(0)
 
-# def generate_frames():
-#     while True:
-#         sucess,frame = camera.read()
-#         if not sucess:
-#             break
-#         else:
-#             ret,buffer = cv2.imencode('.jpg',frame)
-#             frame = buffer.tobytes()
-
-#         yield(b'--frame\r\n'
-#                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 
 @app.route('/', methods = ['GET'])
