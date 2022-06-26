d4ed2b3e9613df51ef83f64d408587b8e5d1a1a5
Dev Patel <68211801+devpatel917@users.noreply.github.com>
2022-06-25 22:37:18 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -8,8 +8,10 @@ app = Flask(__name__)
 def hello_word():
     return render_template('index.html')
 
-
+@app.route('/mask', methods = ['POST', 'GET'])
+def mask_or_no_mask():
+    return render_template('mask.html')
 
 
 if __name__ == "__main__":
-    app.run(debug = True)
+    app.run(debug = True, port = 8000)
