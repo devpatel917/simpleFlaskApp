6415257712f4e982077a2bf85e8946aa096acf2e
Dev Patel <68211801+devpatel917@users.noreply.github.com>
2022-06-25 22:49:28 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
-from flask import Flask, render_template, render_template_string, request, Response
+from flask import Flask, render_template, Response
+import cv2
 
 
 app = Flask(__name__)
+camera = cv2.VideoCapture(0)
+
+def generate_frames():
+    while True:
+        sucess,frame = camera.read()
+        if not sucess:
+            break
+        else:
+            ret,buffer = cv2.imencode('.jpg',frame)
+            frame = buffer.tobytes()
+
+        yield(b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
 
 @app.route('/', methods = ['GET'])
 
@@ -12,6 +27,9 @@ def hello_word():
 def mask_or_no_mask():
     return render_template('mask.html')
 
+@app.route('/video')
+def video():
+    return Response(generate_frames(),mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == "__main__":
     app.run(debug = True, port = 8000)
