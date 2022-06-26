6a4702ff6e4d582e63c34528caa2934b4bde612c
Dev Patel <68211801+devpatel917@users.noreply.github.com>
2022-06-26 00:21:13 -0500

    Update app.py

--- app.py
+++ app.py
@@ -32,4 +32,4 @@ def video():
     return Response(generate_frames(),mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == "__main__":
-    app.run(debug = True, port = 8000)
+    app.run(debug = True)
